781a5fb4f2bbd08abdd7433186e64e6523971652
Даниил Демьяненко <daniil-demyanenko1@mail.ru>
2019-12-12 20:49:35 +0300

    Добавьте файлы проекта.

---
+++ testML/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using TestMLML.Model;
+
+namespace testML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Add input data
+            var input = new ModelInput();
+            // Load model and predict output of sample data
+            ModelOutput result;
+
+            while (true)
+            {
+                input.Message = Console.ReadLine();
+                result = ConsumeModel.Predict(input);
+                Console.WriteLine((result.Prediction ? "Spam":"Not spam") + " \n" + result.Score+"\n");
+            }
+        }
+    }
+}
